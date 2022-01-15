67558b5fd5d78b6d52e755ac3a3aa6555191ca58
raceee <race.peterson@gmail.com>
2022-01-15 16:42:14 -0700

    working on streak

--- link.py
+++ link.py
@@ -1,62 +1,80 @@
 import matplotlib as plt
 import numpy as np
-import datetime
-
+from datetime import date
 class Day:
-    def __init__(self) -> None:
+    def __init__(self, score=0, success=True) -> None:
         self.score = 0
+        self.date = date.today()
+        self.success = success
 
 class Goal:
     def __init__(self, goal_name:str) -> None:
         self.goal_name = goal_name
         self.amnesty = self.set_amnesty()
-        self.goal_chain = [Day()]
-        self.used_amnesty = 0
+        self.amnesty_score = self.amnesty
+        self.goal_chain = [Day(success=None)]
         self.goal_score = 0
     
+    def make_day(self):
+        input_ = str(input("Did you complete today's goal? (y/n) "))
+        if input_ == "y":
+            self.goal_chain.append(Day(score=self.goal_chain[len(self.goal_chain) - 1].score + 1))
+            self.reset_amnesty()
+        elif input_ == "n":
+            if self.amnesty_score != 0:
+                self.amnesty_score -= 1
+                self.goal_chain.append(Day(score=self.goal_chain[len(self.goal_chain) - 1].score, success=False))
+            else:
+                self.goal_chain.append(Day(score=self.goal_chain[len(self.goal_chain) - 1].score - 1, success=False))
+
+    def reset_amnesty(self):
+        self.amnesty_score = self.amnesty
+        print("amnesty score reset")
+
     def set_amnesty(self) -> int:
         return int(input("How many days of amnesty do you want for this goal: "))
     
     def get_score(self) -> int:
         # checks the last block in chain
-        return self.goal_score
+        return self.goal_chain[len(self.goal_chain) - 1].score
     
     def get_amnesty(self) -> int:
         return self.amnesty
     
     def get_used_amnesty(self) -> int:
-        return self.used_amnesty
+        return self.amnesty_score
     
     def get_successes(self) -> int:
-        return
+        return sum([1 for day in self.goal_chain if day.success])
     
     def get_misses(self) -> int:
-        return
+        return len(self.goal_chain) - self.get_successes()
     
     def get_streak(self) -> int:
         return
     
     def get_days_on_goal(self) -> int:
-        return
+        return len(self.goal_chain)
 
     def summary(self) -> None:
-        print("Goal Name: {}\n Current Goal Score: {}\n Amnesty Allowance: {}\n Amnesty Used: {}\n Total Success: {}\n Total Missed: {}\n Current Streak: {}\n Days on Goal: {}" \
+        print("Goal Name: {}\n Current Goal Score: {}\n Amnesty Allowance: {}\n Current Amnesty: {}\n Total Success: {}\n Total Missed: {}\n Current Streak: {}\n Days on Goal: {}" \
         .format(self.goal_name, self.get_score(), self.get_amnesty(), self.get_used_amnesty(), self.get_successes(), self.get_misses(), self.get_streak(), self.get_days_on_goal()))
     
     def vis_goal(self) -> None:
         pass
 
-
 class Profile:
     def __init__(self, name:str) -> None:
         self.name = name
-        self.goals = []
+        self.goals = {}
 
     def new_goal(self, goal_name:str) -> list:
-        return self.goals.append(Goal(goal_name))
+        self.goals[goal_name] = Goal(self.name)
 
 if __name__ == "__main__":
     print("hi")
     race_peterson = Profile("Race Peterson")
     race_peterson.new_goal("Sleep")
-    race_peterson.goals[0].summary()
+    for _ in range(6):
+        race_peterson.goals["Sleep"].make_day()
+    race_peterson.goals["Sleep"].summary()
